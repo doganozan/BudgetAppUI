ba99018fd397acefabef5d2b858b541262711fb9
ozan <45738864+doganozan@users.noreply.github.com>
2020-03-09 06:00:02 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:budgetappui/screens/home_Screen.dart';
 
 void main() => runApp(MyApp());
 
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
